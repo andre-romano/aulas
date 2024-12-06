bbce7e20d9c009c6d554b6f83b855268d06df93a
Andre Madureira <andre@notebook.com>
2024-12-06 11:38:34 -0300

    add projeto

---
+++ redes/PROJETO_02/client.py
@@ -0,0 +1,7 @@
+# TODO
+#  - conexao com o servidor para implementar as funcionalidades e requisitos do projeto
+
+# - cada cliente se comunica com o servidor, que gerenciara a comunicacao entre clientes
+# - cada cliente deve se cadastrar junto ao servidor como um usuario
+# - cada cliente deve poder se comunicar com outro cliente usando o nome de usuario (semelhante ao que ocorre no WhatsApp atraves do numero de telefone)
+# - (OPCIONAL) clientes podem se juntar a grupos multicast (semelhante ao que ocorre no whatsapp)
